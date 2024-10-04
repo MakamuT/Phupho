98764b478765230bc7994284caf225f6fad81a4a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-04 21:54:33 +0200

    feat: :sparkles: adding the constant file to store Navigation items inside of an array of objects

---
+++ src/constants/index.jsx
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: "Home", href: "#" },
+  { label: "About", href: "#" },
+  { label: "Services", href: "#" },
+  { label: "Team", href: "#" },
+  { label: "Testimonials", href: "#" },
+];
